9ebbf4dfa3e119a76652bbd88114aaca9538cfb6
aadvivian <aadvivian.singh@gmail.com>
2015-08-25 16:12:20 -0400

    Initial Commit

---
+++ VxLib/VxLib.h
@@ -0,0 +1,19 @@
+//
+//  VxLib.h
+//  VxLib
+//
+//  Created by Mtoto on 8/25/15.
+//  Copyright © 2015 saadiKompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VxLib.
+FOUNDATION_EXPORT double VxLibVersionNumber;
+
+//! Project version string for VxLib.
+FOUNDATION_EXPORT const unsigned char VxLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VxLib/PublicHeader.h>
+
+
